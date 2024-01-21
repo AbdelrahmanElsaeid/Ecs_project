63ff9a9b0b437257859918147abe190d0d22dc87
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2024-01-21 12:44:24 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -224,11 +224,11 @@ SITE_ID = 1
 # STATIC_URL = "static/"
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-# STATICFILES_DIRS = [
-#     BASE_DIR/ "static",
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS = [
+     BASE_DIR/ "static",
+ ]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
